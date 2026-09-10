87d377c86db930f0d1c31a64ce6b084638a0bd13
Abdul Nafae Imran <nafaeimran123@gmail.com>
2026-09-10 10:29:47 +0500

    Changed camera intial position so it no longer is inside the black hole at the start

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 const unsigned int SCR_WIDTH = 1920;
 const unsigned int SCR_HEIGHT = 1080;
 //Camera Setup
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 10.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
